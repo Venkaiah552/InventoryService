c0661dcae7700a79139e23993470f1aa82b0dc85
Sarita <sarita.170317@gmail.com>
2018-03-02 15:17:39 +0530

    Fix Inventory data

--- src/main/java/com/honeywell/jpa/repository/VendorRepository.java
+++ src/main/java/com/honeywell/jpa/repository/VendorRepository.java
@@ -1,13 +1,8 @@
 package com.honeywell.jpa.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.honeywell.jpa.model.Vendor;
 
 public interface VendorRepository extends JpaRepository<Vendor, Integer>{
-	
-	List<Vendor> findById(int id);
-	
 }
