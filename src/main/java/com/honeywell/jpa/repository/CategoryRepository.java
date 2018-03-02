17d8e9421c928e65d3d206fb812f4445fe8e3dc2
venkaiah552 <venki552@gmail.com>
2018-03-02 11:48:36 +0530

    new Repo for Catgory

---
+++ src/main/java/com/honeywell/jpa/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.honeywell.jpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.honeywell.jpa.model.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
+
+}
