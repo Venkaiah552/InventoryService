8306739907d2ee708f376464fafcb794e3bad27a
venkaiah552 <venki552@gmail.com>
2018-03-02 11:48:04 +0530

    New pojo

---
+++ src/main/java/com/honeywell/jpa/model/Category.java
@@ -0,0 +1,38 @@
+package com.honeywell.jpa.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Category {
+	
+	private int id;
+	private String name;
+	private String prority;
+
+	public Category() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPrority() {
+		return prority;
+	}
+	public void setPrority(String prority) {
+		this.prority = prority;
+	}
+	
+	enum Size{
+		S,M,L,XL,XXL;
+	}
+
+}
